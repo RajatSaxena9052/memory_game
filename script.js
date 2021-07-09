00d7836c418f8384f96068eb43eea4f97120c9ce
rajat-saxena-16 <rajat.saxena.16@mountblue.tech>
2021-07-09 15:14:35 +0530

    second

--- script.js
+++ script.js
@@ -1,31 +1,148 @@
 const gameContainer = document.getElementById("game");
 
-const startButton = document.getElementById("startButton");
-const restartButton = document.getElementById("restartButton");
-const moves = document.getElementById("moves")
+const mem = document.getElementById("mem")
+const select = document.getElementById("select");
 
-startButton.addEventListener("click", () => {
-  startButton.style.display = "none";
+const homeButton = document.getElementById("homeButton");
+
+const easyButton = document.getElementById("easyButton");
+const mediumButton = document.getElementById("mediumButton");
+const hardButton = document.getElementById("hardButton");
+const restartButton = document.getElementById("restartButton");
+const moves = document.getElementById("moves");
+const score = document.getElementById("score");
+
+easyButton.addEventListener("click", () => {
+  mem.style.fontSize = "34px";
+  let shuffledColors = shuffle(COLORS.slice(0, 4))
+  select.style.display = "none";
+
+  createDivsForColors(shuffledColors);
+  homeButton.style.display = "block";
+  easyButton.style.display = "none";
+  mediumButton.style.display = "none";
+  hardButton.style.display = "none";
   restartButton.style.display = "block";
   gameContainer.style.display = "block";
+  mv.style.display = "block";
+  sc.style.display = "block";
 })
-/*
-restartButton.addEventListener("click",()=>{
 
-})*/
+mediumButton.addEventListener("click", () => {
+  select.style.display = "none";
+  mem.style.fontSize = "34px";
 
-const COLORS = [
+  let shuffledColors = shuffle(COLORS.slice(0, 8))
+
+  createDivsForColors(shuffledColors);
+
+  homeButton.style.display = "block";
+  easyButton.style.display = "none";
+  mediumButton.style.display = "none";
+  hardButton.style.display = "none";
+  restartButton.style.display = "block";
+  gameContainer.style.display = "block";
+  mv.style.display = "block";
+  sc.style.display = "block";
+})
+
+hardButton.addEventListener("click", () => {
+  select.style.display = "none";
+  mem.style.fontSize = "44px";
+
+  let shuffledColors = shuffle(COLORS);
+
+  createDivsForColors(shuffledColors.slice(0, 16));
+
+  homeButton.style.display = "block";
+  easyButton.style.display = "none";
+  mediumButton.style.display = "none";
+  hardButton.style.display = "none";
+  restartButton.style.display = "block";
+  gameContainer.style.display = "block";
+  mv.style.display = "block";
+  sc.style.display = "block";
+})
+
+
+
+
+homeButton.addEventListener("click", () => {
+  easyButton.style.display = "block";
+  mediumButton.style.display = "block";
+  hardButton.style.display = "block";
+  restartButton.style.display = "none";
+  gameContainer.style.display = "none";
+  moves.style.display = "none";
+  score.style.display = "none";
+  location.reload()
+
+})
+
+restartButton.addEventListener("click", () => {
+
+  restartButton.style.display = "block";
+  gameContainer.style.display = "block";
+  location.reload()
+
+})
+
+/* const COLORS = [
+  "red",
   "red",
   "blue",
+  "blue",
   "green",
+  "green",
+  "orange",
   "orange",
   "purple",
+  "purple",
+  "red",
   "red",
   "blue",
+  "blue",
+  "green",
   "green",
   "orange",
+  "orange",
+  "purple",
   "purple"
-];
+]; */
+
+const COLORS = [
+  "ant.jpeg",
+  "ant.jpeg",
+  "blackpanther.jpeg",
+  "blackpanther.jpeg",
+  "captain.jpg",
+  "captain.jpg",
+  "doctorstrange.jpeg",
+  "doctorstrange.jpeg",
+  "falcon.jpeg",
+  "falcon.jpeg",
+  "Groot.jpg",
+  "Groot.jpg",
+  "hawkeye.jpeg",
+  "hawkeye.jpeg",
+  "Hulk.jpg",
+  "Hulk.jpg",
+  "ironman.jpg",
+  "ironman.jpg",
+  "spiderman.jpg",
+  "spiderman.jpg",
+  "thanos.jpg",
+  "thanos.jpg",
+  "thor.jpeg",
+  "thor.jpeg",
+  "vision.jpeg",
+  "vision.jpeg",
+  "warmachine.jpeg",
+  "warmachine.jpeg",
+  "widow.jpeg",
+  "widow.jpeg"
+]
+
 
 // here is a helper function to shuffle an array
 // it returns the same array with values shuffled
@@ -50,7 +167,7 @@ function shuffle(array) {
   return array;
 }
 
-let shuffledColors = shuffle(COLORS);
+// let shuffledColors = shuffle(COLORS);
 
 // this function loops over the array of colors
 // it creates a new div and gives it a class with the value of the color
@@ -58,8 +175,14 @@ let shuffledColors = shuffle(COLORS);
 function createDivsForColors(selectedColorArray) {
   let id = 1;
   for (let color of selectedColorArray) {
+    console.log(selectedColorArray.length);
+
     // create a new div
     const newDiv = document.createElement("div");
+    // const imgDiv = document.createElement("img");
+    // imgDiv.appendChild(newDiv);
+
+    newDiv.style.backgroundColor = "#3E215D";
 
     // give it a class attribute for the value we are looping over
     newDiv.classList.add(color);
@@ -76,13 +199,29 @@ function createDivsForColors(selectedColorArray) {
 }
 
 
+const calculateScore = () => {
+  if (numberOfMoves === 10) {
+    return 100;
+  } else {
+    return 100 - ((numberOfMoves - 10) * 2.5)
+  }
+
+}
+
+
+
+
 //comparing cards here and incrmenting winning number
-let numberOfWins = 0;
 function compareCards([firstCard, secondCard]) {
 
   if ((firstCard.classList[0] === secondCard.classList[0]) && (firstCard.id !== secondCard.id)) {
     return true;
-  } else {
+  }
+  else if ((firstCard.classList[0] === secondCard.classList[0]) && (firstCard.id == secondCard.id)) {
+
+    return undefined;
+  }
+  else {
     return false;
   }
 
@@ -90,7 +229,7 @@ function compareCards([firstCard, secondCard]) {
 
 
 
-let count = 0, selectedColorArray = [], finishGame = [], lockBoard = false, numberOfMoves = 0;
+let count = 0, selectedColorArray = [], finishGame = [], lockBoard = false, numberOfMoves = 0, scores = 0;
 
 // TODO: Implement this function!
 function handleCardClick(event) {
@@ -105,14 +244,15 @@ function handleCardClick(event) {
   count++;
   console.log("you clicked", event.target, count);
 
+  let pre = "";
 
   if (count <= 2) {
-    numberOfMoves++;
-    moves.textContent = numberOfMoves;
 
     let colorClicked = event.target.classList[0];
 
-    event.target.style.backgroundColor = colorClicked;
+    // event.target.style.backgroundColor = colorClicked;
+    event.target.style.backgroundImage = `url(./marvel/${colorClicked})`;
+    // imgDiv.src = `./marvel/${colorClicked}`;
 
     selectedColorArray.push(event.target)
   }
@@ -120,26 +260,28 @@ function handleCardClick(event) {
   if (count === 2) {
 
 
-    moves.textContent = numberOfMoves;
-
     let isMatched = compareCards(selectedColorArray);
 
     if (isMatched === true) {
+      scores++;
+      score.textContent = scores;
 
-      numberOfMoves++;
+      numberOfMoves += 2;
+      moves.textContent = numberOfMoves;
+
 
       let totalNumberOfColors = COLORS.length / 2;
 
-      if (numberOfWins === totalNumberOfColors) {
+      if (scores === totalNumberOfColors) {
 
         selectedColorArray.forEach(item => {
           item.removeEventListener("click", handleCardClick);
         });
 
-
-        setTimeout(() => {
-          alert("Game won", location.reload());
-        }, 500);
+        let SCORE = calculateScore();
+        score.innerText = SCORE;
+        score.style.display = "block";
+        alert("Game won", "your score is:", SCORE);
 
       }
 
@@ -151,12 +293,31 @@ function handleCardClick(event) {
 
       console.log("cards MATCHED");
 
-    } else {
+    } else if (isMatched === undefined) {
+      selectedColorArray.forEach(s => {
+
+        setTimeout(() => {
+          s.style.backgroundColor = "#3E215D";
+          s.style.backgroundImage = "none";
+
+          lockBoard = false;
+        }, 1000);
+
+      });
+
+    }
+    else {
+
+      numberOfMoves += 2;
+      moves.textContent = numberOfMoves;
+
+
       lockBoard = true;
       selectedColorArray.forEach(s => {
 
         setTimeout(() => {
-          s.style.backgroundColor = "white";
+          s.style.backgroundColor = "#3E215D";
+          s.style.backgroundImage = "none";
           lockBoard = false;
         }, 1000);
 
@@ -167,7 +328,7 @@ function handleCardClick(event) {
     selectedColorArray = [];
   }
 
-  console.log(document.querySelector("div"), "hello form other side")
+  console.log(document.querySelector("div"), "hello form other side", moves.textContent)
 
 
 }
@@ -175,4 +336,3 @@ function handleCardClick(event) {
 
 
 // when the DOM loads
-createDivsForColors(shuffledColors);
